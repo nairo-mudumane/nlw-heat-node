1ab84190e0ac5adc520b7824342f97ef5fa47dd8
nairoMudumane <nazirmudumane@gmail.com>
2021-10-24 07:09:09 -0700

    bug: no response on authenticate route@

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
+import { router } from './Routes';
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(express.json);
+app.use(router);
 app.get('/github', (req, res) => {
   res.redirect(
     `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
